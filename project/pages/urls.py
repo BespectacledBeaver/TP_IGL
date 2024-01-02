987c418dc0cfb04ae19dec7abc0717370acf1c33
MahdiTir <128150343+MahdiTir@users.noreply.github.com>
2024-01-03 02:26:32 +0400

    Update urls.py

--- project/pages/urls.py
+++ project/pages/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from .views import *
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('signup', views.signup, name='signup'),
-    path('signin', views.signin, name='signin'),
-    path('signout', views.signout, name='signout'),
+    path('',Signupview.as_view()) ,
+    path('login',Loginview.as_view()),
 ]
