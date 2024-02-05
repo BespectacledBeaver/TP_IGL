a9df1200cd752fbc21d19fe359a0264c516626e7
MahdiTir <128150343+MahdiTir@users.noreply.github.com>
2024-02-05 15:26:46 +0400

    update searchapp

--- project/searchApp/views.py
+++ project/searchApp/views.py
@@ -4,13 +4,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from articleManagementAPP.models import Article
+from userManagementApp.models import User , FavoriteArticle
 from articleManagementAPP.serializers import ArticleSerializer
 from django.db.models import Q
 
-
-from rest_framework.views import APIView 
-from articleManagementAPP.models import Article
-from rest_framework.response import Response 
 
 # Create your views here.
 
@@ -19,18 +16,36 @@ from rest_framework.response import Response
 class Sentarticlesview(APIView):
 
     def post(self , request): 
-        articles = Article.objects.all().values('id','title', 'publication_date', 'authors')
-        articles = articles.order_by('-publication_date')
-        articles_list = list(articles)
+        userid = request.data.get("userid")
+        if userid is not None:
+            user = User.objects.get(id=userid)
+
+        # Récupérer tous les articles avec une indication sur s'ils sont favoris ou non pour l'utilisateur actuel
+        articles = []
+        all_articles = Article.objects.all().values('id', 'title', 'publication_date', 'authors')
+        for article_data in all_articles:
+            article_id = article_data['id']
+            is_favorite = FavoriteArticle.objects.filter(user=user, article_id=article_id).exists()
+            article_data['is_favorite'] = is_favorite
+            articles.append(article_data)
+
+        # Trier les articles par date de publication
+        articles = sorted(articles, key=lambda x: x['publication_date'], reverse=True)
+        list_articles =  list(articles)
     
-        #response = Response(articles_list)
-        return Response({'articles': articles_list})
+        return Response(list_articles)
     
         
 
 
 class SearchArticlesView(APIView):
     def post(self, request):
+
+        userid = request.data.get("userid")
+        if userid is not None:
+            user = User.objects.get(id=userid)
+
+
         search_options = request.data.get('search_options', [])
         keywords = request.data.get('keywords', [])
         start_date = request.data.get('start_date', [])
@@ -53,18 +68,18 @@ class SearchArticlesView(APIView):
             elif option == 'institutions':
                 queryset |= Article.objects.filter(institutions__icontains=keywords)
 
-        filtered_results = []
+        filtered_results = Article.objects.none()
         for article in queryset:    
             if (not start_date or option['publication_date'] >= start_date) and \
                (not end_date or option['publication_date'] <= end_date):
-                filtered_results.append(article)
+                filtered_results |= article
         
 
          # Sort the queryset by publication_date in descending order (most recent first)
         filtered_results = filtered_results.order_by('-publication_date') # - is for descending order 
         
-        articles = filtered_results.objects.all().values('id','title', 'publication_date', 'authors')
+        articles = filtered_results.all().values('id','title', 'publication_date', 'authors')
         articles_list = list(articles)
-        response = Response(articles_list)
-        return response
+        return Response(articles_list)
+        
     
